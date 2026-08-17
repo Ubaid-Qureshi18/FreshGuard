69ccdaab9111added600eef83882e045ac6acb5d
Ubaid Qureshi <ubaidentrance000@gmail.com>
2026-08-17 17:23:58 +0530

    FreshGuard Stability & ErrorBoundary Fixes: Wrapped React tree with ErrorBoundary, verified zero build errors across client & server

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 
 // Load Inter font
@@ -11,6 +12,8 @@ document.head.appendChild(link);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 )
